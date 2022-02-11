ad2c8c344bf57564b09a966e3820b436c4f30ce0
heavyrisem <pyo1748@gmail.com>
2022-02-12 00:51:20 +0900

    fix: 디버그 메세지 비활성화

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 
 // Get Code From Github master branch
-const jsoup = org.jsoup.Jsoup.connect("https://raw.githubusercontent.com/Heavyrisem/NotionKakao_Bot/master/main.js").get();
+const jsoup = org.jsoup.Jsoup.connect("https://raw.githubusercontent.com/Heavyrisem/NotionKakao_Bot/master/main.js" + "?date=" + Date.now()).get();
 jsoup.outputSettings().prettyPrint(false);
 const CODE_jsoup = jsoup.wholeText();
 Log.debug(CODE_jsoup);
